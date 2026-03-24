867888b601fb46422824cb1d494e21f07267d772
ZeroOxygene <runigokilian@gmail.com>
2026-03-24 13:48:05 +0100

    merged menu

--- Model game Tileset/Game/Game.c
+++ Model game Tileset/Game/Game.c
@@ -5,13 +5,13 @@ Game game;
 
 void LoadGame(void)
 {
-	LoadMap("Map");
+	//LoadMap("Map");
 
-	sfFloatRect collision = GetMapCollision(0);
-	printf("%.2f %.2f %.2f %.2f\n", collision.left, collision.top, collision.width, collision.height);
+	//sfFloatRect collision = GetMapCollision(0);
+	//printf("%.2f %.2f %.2f %.2f\n", collision.left, collision.top, collision.width, collision.height);
 
-	Trigger trigger = GetMapTrigger(0);
-	printf("%s %.2f %.2f %.2f %.2f\n", trigger.name, trigger.left, trigger.top, trigger.width, trigger.height);
+	//Trigger trigger = GetMapTrigger(0);
+	//printf("%s %.2f %.2f %.2f %.2f\n", trigger.name, trigger.left, trigger.top, trigger.width, trigger.height);
 
 }
 
@@ -57,11 +57,11 @@ void UpdateGame(float _dt)
 
 void DrawGame(sfRenderWindow* _renderWindow)
 {
-	DrawMap(_renderWindow);
+	//DrawMap(_renderWindow);
 }
 
 void CleanupGame(void)
 {
-	CleanupMap();
+	//CleanupMap();
 
 }
