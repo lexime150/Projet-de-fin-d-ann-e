aa6221898e6b343fbc9640efd6e13531671ee667
ZeroOxygene <runigokilian@gmail.com>
2026-04-10 10:51:31 +0200

    test

--- Model game Tileset/Game/Player.c
+++ Model game Tileset/Game/Player.c
@@ -665,7 +665,7 @@ void MovePlayer(float _dt)
 		}
 	}
 }
-
+/////////////////////////////////////////////////////////////////////////////
 void CollisionPlayerPlatformsX(float _dx)
 {
 	sfBool sKey = sfKeyboard_isKeyPressed(sfKeyS);
